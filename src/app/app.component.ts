b46e13286067c1d0c56f4a947f8e3cd546a89aee
Kyle Lauritzen <kyle.lauritzen@sophos.com>
2017-05-31 17:02:48 -0600

    edited text, installed packages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  selector: 'todo-list-app',
+  template: `<h1>To-do list for {{name}}</h1>`,
 })
 export class AppComponent  { name = 'Angular'; }
